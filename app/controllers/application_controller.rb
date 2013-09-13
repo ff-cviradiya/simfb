666c7513526ea02b33e87112b06d7d0f241224fe
Chirag Viradiya <chirag.viradiya@firstfuel.com>
2013-09-13 13:19:09 +0530

    Fun before filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :current_user
+
+
+  def print_request_start
+    puts "Request started at #{Time.now}"
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+
+  before_filter :print_request_start
+
   def index
     # QUALITY_BREACH - Unnecessary code.
     10.times do
