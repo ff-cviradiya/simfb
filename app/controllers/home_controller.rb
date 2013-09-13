3964fe3fbd9c0cb1aa88663ca8f2a9060b3a621c
Chirag Viradiya <chirag.viradiya@firstfuel.com>
2013-09-13 13:03:40 +0530

    Added unnecessary code.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
+    # This code is to understand if code quality metric catches it
+    10.times do
+      2 * 2
+    end
     @users = User.all
     @status = Status.new
+
   end
 end
